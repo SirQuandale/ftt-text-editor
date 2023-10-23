71496700d239fb63d6015d0094ec6901fe66cfad
SirQuandale <302685723@student.rocmondriaan.nl>
2023-10-23 15:30:08 +0200

    made the paper and menu bar

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FTT Text Editor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <div class="paper-container">
+        <form method="post">
+            <textarea type="text" name="text"></textarea>
+        </form>
+    </div>
+</body>
+
+</html>
